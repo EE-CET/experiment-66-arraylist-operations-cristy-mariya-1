4a6c438b72092fe1f57db08314f7aa1ce14199d9
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-09 13:43:38 +0530

    Update ArrayListOperations.java

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -1,10 +1,32 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class ArrayListOperations {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<String> list = new ArrayList<>();
+
+        // Read items
+        for (int i = 0; i < n; i++) {
+            list.add(sc.next());
+        }
+
+        // Sort items alphabetically
+        Collections.sort(list);
+
+        // Search item
+        String searchItem = sc.next();
+        boolean found = list.contains(searchItem);
+
+        // Output
+        System.out.println("Sorted Items: " + list);
+        if (found) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not Found");
+        }
+
+        sc.close();
     }
 }
