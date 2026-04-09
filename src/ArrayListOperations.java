0ae2a6e39c70d1709753fcfd0977590ebd44bbb7
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-09 18:06:33 +0530

    Update ArrayListOperations.java

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -7,21 +7,16 @@ public class ArrayListOperations {
         int n = sc.nextInt();
         ArrayList<String> list = new ArrayList<>();
 
-        // Read items
         for (int i = 0; i < n; i++) {
             list.add(sc.next());
         }
 
-        // Sort alphabetically
         Collections.sort(list);
 
-        // Read search item
         String searchItem = sc.next();
 
-        // Print sorted list
         System.out.println("Sorted Items: " + list);
 
-        // Check existence
         if (list.contains(searchItem)) {
             System.out.println("Found");
         } else {
